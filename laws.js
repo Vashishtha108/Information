91070c36d3dc3c40d4d52638b2ad92aeea2bf283
Vashishtha108 <161292366+Vashishtha108@users.noreply.github.com>
2024-07-01 08:05:04 +0530

    Update laws.js

--- laws.js
+++ laws.js
@@ -2,5 +2,5 @@ const _ = {
     "Coaching institutes" : "If cutoff of the exam is cleard, then no refund, else 36.63% refund is applicable, if promise/statement that gives some assurity of clearning cut-off, then 18% extra refund.",
     "Hostel" : "national hostel rules, pay-as-you-go services, full refund of security deposit, no contract payment is allowed, no individual hostel rules",
     "Students" : "seperate study applications and seperate study device for the students, auto-lock features for manipulation, no voilent content for students.",    
-    "" : "",
+    "" : "No use of non-lethal white and red clothes, burqa for woman with content promotion. ",
 };
