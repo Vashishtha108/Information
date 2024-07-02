8469e9df01cfbfa1e99dcdb7f5df6d3a0de71769
Vashishtha108 <161292366+Vashishtha108@users.noreply.github.com>
2024-07-02 14:09:22 +0530

    Update om_wives.js

--- om_wives.js
+++ om_wives.js
@@ -1,18 +1,22 @@
 const _values = {
 
     // Wives are imported as fantasies.
-    // Imported as sattava guna to bhakti 
-    "Maa Parvati ji" : "Aarohishree",
-    "Maa Mahalakshmi ji" : "Vidhishree",
-    "Maa Mahakali ji" : "Vaahishree",
-    "Maa Vindyavasini ji" : "Vanshishkashree",
-    "Maa Ganga ji" : "Vanshishree",
+    // Imported as sattava guna to bhakti.
+
+    "Maa Parvati ji" : "Aarohishree # shraddha kapoor",
+    "Maa Mahalakshmi ji" : "Vidhishree # vardha (amisha) + amber heard",
+    "Maa Mahakali ji" : "Vaahishree # Hande ercel",
+    "Maa Vindyavasini ji" : "Vanshishkashree # ogze yagiz",
+    "Maa Ganga ji" : "Vanshishree # vanshika + sunny", 
+    "Maa Durga ji" : "Vaanishree # zendaya", 
+    
+
   
     // Imported as sattava guna to bhakti
     "Bhagwan Hanuman ji" : {
         "sita" : "aarohi mehwish",
         "raam" : "utkarsha vaibhava viraat connor utsava uttama vijaya",
-        "vaani vedna vinnie" : "disha patani nancy momoland jessica alba",
+        " vedna vinnie" : "disha patani nancy momoland jessica alba",
         "vanshita vinci yummie" : "kristen stewart burcu ozberk bella poarch",
         "vaahi vanshishka viha" : "hande ercel ogze yagiz ogze gurer",
         "vahni vanilla vahvi" : "nina dobrev samantha ruth prabhu",    
