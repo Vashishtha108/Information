caccf6cc12abece7b7b872bf30210f6bbe9eb1fa
Vashishtha108 <161292366+Vashishtha108@users.noreply.github.com>
2024-07-31 09:33:38 +0530

    Update life.js

--- life.js
+++ life.js
@@ -2,7 +2,7 @@ const _ = {
  "Do" : { 
     "Technology block" : "all life", 
     "Poison block" : "all life", 
-    "" : "", 
- "" : "",
+    "Morning exercises" : "all life", 
+    "Evening walk" : "all life",
  "" : "",
 };
