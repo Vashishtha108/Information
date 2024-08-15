f95cb057221b401e91a73bafca1f78b8ed88bc93
Vashishtha108 <161292366+Vashishtha108@users.noreply.github.com>
2024-08-15 19:03:23 +0530

    Update

---
+++ lustanddirtiness.js
@@ -0,0 +1,3 @@
+const _ = { 
+"consious way" : "put the face of your mother and sister and rhyme of the consious definition of the shining factor of the females of the world.", 
+"my way" : "unset and unallocate the vibrations energies via desires via raag of sense organs of the female orgamns like curves, eyes, hairs, hips, breasts, and red dress, worship Lord radha krishna's name during the sensing process, eventually u all respect every woman}, 
