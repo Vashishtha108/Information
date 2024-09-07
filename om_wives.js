ba0a60bc46a3b364a495c41897c26ab4118d6b3d
Vashishtha108 <161292366+Vashishtha108@users.noreply.github.com>
2024-09-07 17:52:05 +0530

    Update om_wives.js

--- om_wives.js
+++ om_wives.js
@@ -7,7 +7,7 @@ const _values = {
     "Maa Lakshmi ji" : "Vidhishree # vardha (amisha) + amber heard",
     "Maa Mahakali ji" : "Vaahishree # Hande ercel",
     "Maa Vindyavasini ji" : "Vanshishkashree # ogze yagiz",
-    "Maa Ganga ji" : "Vanshishree # vanshika ", 
+    "Maa Ganga ji" : "Vanshishree # vanshika alexandria daddario", 
     "Maa ji" : "Vanshitashree # vanshita + kristen stewart",
     "Maa Chandika ji" : "Vednashree # vedna + Nancy momoland", 
     "Bhagwan Baman and Maa Bamani ji" : "Vaanishree # Vaani + Zendaya", 
