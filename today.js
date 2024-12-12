044ecf83e8b6a430ffbcc4efbf9d2fb6516a8103
Vashishtha108 <161292366+Vashishtha108@users.noreply.github.com>
2024-12-12 22:23:45 +0530

    Update today.js

--- today.js
+++ today.js
@@ -41,6 +41,6 @@ Awar _ = {
         "making female friends", 
         "listening bollywood songs", 
         "watching digital creative content",
-        "eating sugar, salt, onion food"
+        "eating sugar, salt, onion, oily food"
     ],
 };
